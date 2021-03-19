4a2880436baa90e95f3dae1d83e849355091d592
caojiayuan <cjy632258@hotmail.com>
2021-03-19 10:50:09 +0800

    update redis

--- workflow-engine/model/redis.go
+++ workflow-engine/model/redis.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"context"
 	"fmt"
 	"time"
 
@@ -17,13 +18,14 @@ var RedisOpen = false
 // SetRedis 设置redis
 func SetRedis() {
 	fmt.Println("-------启动redis--------")
+	ctx := context.Background()
 	if conf.RedisCluster == "true" {
 		clusterIsOpen = true
 		redisClusterClient = redis.NewClusterClient(&redis.ClusterOptions{
 			Addrs:    []string{conf.RedisHost + ":" + conf.RedisPort},
 			Password: conf.RedisPassword,
 		})
-		pong, err := redisClusterClient.Ping().Result()
+		pong, err := redisClusterClient.Ping(ctx).Result()
 		if err != nil {
 			fmt.Printf("------------连接 redis cluster：%s 失败,原因：%v\n", conf.RedisHost+":"+conf.RedisPort, err)
 		}
@@ -34,7 +36,7 @@ func SetRedis() {
 			Addr:     conf.RedisHost + ":" + conf.RedisPort,
 			Password: conf.RedisPassword,
 		})
-		pong, err := redisClient.Ping().Result()
+		pong, err := redisClient.Ping(ctx).Result()
 		if err != nil {
 			fmt.Printf("------------连接 redis：%s 失败,原因：%v\n", conf.RedisHost+":"+conf.RedisPort, err)
 		}
@@ -45,16 +47,20 @@ func SetRedis() {
 
 // RedisSetVal 将值保存到redis
 func RedisSetVal(key, value string, expiration time.Duration) error {
+	ctx := context.Background()
+
 	if clusterIsOpen {
-		return redisClusterClient.Set(key, value, expiration).Err()
+		return redisClusterClient.Set(ctx, key, value, expiration).Err()
 	}
-	return redisClient.Set(key, value, expiration).Err()
+	return redisClient.Set(ctx, key, value, expiration).Err()
 }
 
 // RedisGetVal 从redis获取值
 func RedisGetVal(key string) (string, error) {
+	ctx := context.Background()
+
 	if clusterIsOpen {
-		return redisClusterClient.Get(key).Result()
+		return redisClusterClient.Get(ctx, key).Result()
 	}
-	return redisClient.Get(key).Result()
+	return redisClient.Get(ctx, key).Result()
 }
